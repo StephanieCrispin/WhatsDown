7a3add799ea359c689a1acb26b0b268131e930c0
StephanieMfon <okpomfonstephanie@gmail.com>
2024-07-17 14:20:06 +0100

    feat(home): formatted page grid layout

---
+++ client-1/src/components/logoSearch/LogoSearch.jsx
@@ -0,0 +1,7 @@
+import "./LogoSearch.css";
+
+const LogoSearch = () => {
+  return <div className="LogoSearch">LogoSearch nsmd</div>;
+};
+
+export default LogoSearch;
---
+++ client-1/src/components/profileside/ProfileSide.jsx
@@ -0,0 +1,12 @@
+import LogoSearch from "../logoSearch/LogoSearch";
+import "./ProfileSide.css";
+
+const ProfileSide = () => {
+  return (
+    <div className="ProfileSide">
+      <LogoSearch />
+    </div>
+  );
+};
+
+export default ProfileSide;
