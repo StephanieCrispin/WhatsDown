f6c541f7906e0dc05e4c8ae31e2bf4869a4f8a5c
StephanieMfon <okpomfonstephanie@gmail.com>
2024-07-18 13:37:17 +0100

    feat:built post section top

--- client-1/src/components/PostShare/PostShare.jsx
+++ client-1/src/components/PostShare/PostShare.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 import "./PostShare.css";
 import ProfileImage from "../../img/profileImg.jpg";
-
+import { UilScenery } from "@iconscout/react-unicons";
+import { UilPlayCircle } from "@iconscout/react-unicons";
+import { UilLocationPoint } from "@iconscout/react-unicons";
+import { UilSchedule } from "@iconscout/react-unicons";
+import { UilTimes } from "@iconscout/react-unicons";
 const PostShare = () => {
   return (
     <div className="PostShare">
       <img src={ProfileImage} alt="" />
+
       <div>
         <input type="text" placeholder="What's happening" />
+        <div className="postOptions">
+          <div
+            className="option"
+            style={{ color: "var(--photo)" }}
+            // onClick={() => imageRef.current.click()}
+          >
+            <UilScenery />
+            Photo
+          </div>
+
+          <div className="option" style={{ color: "var(--video)" }}>
+            <UilPlayCircle />
+            Video
+          </div>
+          <div className="option" style={{ color: "var(--location)" }}>
+            <UilLocationPoint />
+            Location
+          </div>
+          <div className="option" style={{ color: "var(--shedule)" }}>
+            <UilSchedule />
+            Shedule
+          </div>
+        </div>
       </div>
-      <div className="postOptions"></div>
     </div>
   );
 };
