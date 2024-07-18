441dcc4cde0794eb004bcca87bfa69c09ac0498d
StephanieMfon <okpomfonstephanie@gmail.com>
2024-07-18 11:46:16 +0100

    feat(client):profile setup

--- client-1/src/components/profileside/ProfileSide.jsx
+++ client-1/src/components/profileside/ProfileSide.jsx
@@ -1,10 +1,12 @@
 import LogoSearch from "../logoSearch/LogoSearch";
+import ProfileCard from "../profilecard/ProfileCard";
 import "./ProfileSide.css";
 
 const ProfileSide = () => {
   return (
     <div className="ProfileSide">
       <LogoSearch />
+      <ProfileCard />
     </div>
   );
 };
