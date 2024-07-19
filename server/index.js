c1ece5e722e2f4c29c3e3df8d6c3222383fc2874
StephanieMfon <okpomfonstephanie@gmail.com>
2024-07-19 22:10:14 +0100

    feat: setup sign up auth

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import AuthRoute from "./routes/AuthRoute.js";
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+const PORT = process.env.PORT;
+
+const CONNECTION = process.env.MONGODB_CONNECTION;
+mongoose
+  .connect(CONNECTION)
+  .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+// usage of routes
+app.use("/auth", AuthRoute);
